2e48bdc9bf0b6d3f0a6f2c2ba0588ea295ffe116
priyab99 <priya.bd.9911@gmail.com>
2023-05-12 20:18:11 +0600

    extra section done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../../Banner/Banner';
 import Welcome from '../../Welcome/Welcome';
+import Speciality from '../../Speciality/Speciality';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Welcome></Welcome>
             <h2 className='text-center mt-5'>Our Chefs</h2>
             <Banner></Banner>
+            <Speciality></Speciality>
         </div>
     );
 };
---
+++ src/pages/Speciality/Speciality.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import img1 from '../../assets/images/img1.jpeg'
+import img2 from '../../assets/images/img2.jpeg'
+import img3 from '../../assets/images/img3.jpeg'
+import { Carousel } from 'react-bootstrap';
+
+
+const Speciality = () => {
+    return (
+        <div>
+            <h2 className='text-center mt-5'>Most Demanding Dishes Right Now</h2>
+            <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Samosa</h3>
+          <p>Served Hot and Fresh.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Biriyani</h3>
+          <p>Vegetable Biriyani, Chicken Biriyani , Mutton Biriyani.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={img3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Mixed Veged Vonda</h3>
+          <p>
+            Yummy and Soft
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+            
+        </div>
+    );
+};
+
+export default Speciality;
