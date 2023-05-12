9acd20e13511fb397442255189c2dcc591c0e2a5
priyab99 <priya.bd.9911@gmail.com>
2023-05-12 19:22:06 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('enviornment variable',import.meta.env.VITE_apikey)
 const firebaseConfig = {
-  apiKey: "AIzaSyB_Buyb-Gd1F1B8Z9oX5xu14JCuSpIIJ0o",
-  authDomain: "assignment-ten-6a2ed.firebaseapp.com",
-  projectId: "assignment-ten-6a2ed",
-  storageBucket: "assignment-ten-6a2ed.appspot.com",
-  messagingSenderId: "201167180680",
-  appId: "1:201167180680:web:fcaabfb6110461cf35a03d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
